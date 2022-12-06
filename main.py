5891e30c41f52266347af443d34ea95df040bb11
skarkhanis95 <62596946+skarkhanis95@users.noreply.github.com>
2022-12-06 16:56:41 +0530

    function test completed

--- main.py
+++ main.py
@@ -1,21 +1,4 @@
 import random
-
-word_list = ["baboon","camel","ardvark","siddharth"]
-# choose random word from the pre-defined list above
-chosen_word = random.choice(word_list)
-#testing code
-print(f"{chosen_word}")
-
-#generating display
-display = []
-len_of_display = len(chosen_word)
-
-for a in range(1,len_of_display+1):
-    display.append("_")
-
-print(display)
-
-
 # 6 stages
 stages = ['''
   +---+
@@ -74,18 +57,45 @@ stages = ['''
 =========
 ''']
 
-end_of_game = False
+lives = 6
+word_list = ["baboon","camel","ardvark","siddharth"]
+# choose random word from the pre-defined list above
+chosen_word = random.choice(word_list)
+#testing code
+print(f"{chosen_word}")
 
+#generating display
+display = []
+len_of_display = len(chosen_word)
+
+for a in range(1,len_of_display+1):
+    display.append("_")
+
+print(display)
+
+end_of_game = False
+life_lost = False
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
 # use the loop to check if guessed (guess) letter matched the randomnly chosed word
     for position in range(len_of_display):
         letter = chosen_word[position]
         if letter == guess:
-            display[position] = letter
+          display[position] = letter
+        
     
+    if guess not in chosen_word:
+      print(stages[lives])
+      lives -= 1
+      
     print(display)
-    if "_" not in display:
+    if "_" not in display and lives > 0:
         end_of_game = True
         print("You Won!")
+    elif "_" in display and lives>0:
+        end_of_game = False
+        #print("Try Again")
+    elif "_" in display and lives < 0:
+        end_of_game = True
+        print("You Lost")
         
