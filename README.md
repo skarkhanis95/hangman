97bc30a398d21beee0aecda2b1857f47e373ad5b
skarkhanis95 <62596946+skarkhanis95@users.noreply.github.com>
2022-12-06 16:08:01 +0530

    step 3
    Added readme
    added stages
    added while loop to end game

---
+++ README.md
@@ -0,0 +1,1 @@
+# Hangman Game with Basic Python
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ word_list = ["baboon","camel","ardvark","siddharth"]
 # choose random word from the pre-defined list above
 chosen_word = random.choice(word_list)
 #testing code
-#print(f"{chosen_word}")
+print(f"{chosen_word}")
 
 #generating display
 display = []
@@ -14,13 +14,78 @@ for a in range(1,len_of_display+1):
     display.append("_")
 
 print(display)
-# take input of letter from user and convert to lower
-guess = input("Guess a letter: ").lower()
 
+
+# 6 stages
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+end_of_game = False
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
 # use the loop to check if guessed (guess) letter matched the randomnly chosed word
-for position in range(len_of_display):
-    letter = chosen_word[position]
-    if letter == guess:
-        display[position] = letter
+    for position in range(len_of_display):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+    
+    print(display)
+    if "_" not in display:
+        end_of_game = True
+        print("You Won!")
         
-print(display)
