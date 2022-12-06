15223aee6ce1a53fac2726a6d78ce435220ec278
skarkhanis95 <62596946+skarkhanis95@users.noreply.github.com>
2022-12-06 15:45:38 +0530

    display setup
    1. Added Display
    2. Replaced letters of display with guesses letter

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import random
 
-word_list = ["baboon","camel","ardvark"]
-
+word_list = ["baboon","camel","ardvark","siddharth"]
+# choose random word from the pre-defined list above
 chosen_word = random.choice(word_list)
+#testing code
+#print(f"{chosen_word}")
 
-guess = input("Guess a letter: ")
+#generating display
+display = []
+len_of_display = len(chosen_word)
 
-for x in chosen_word:
-    if x == guess:
-        print(True)
-    else:
-        print(False)
+for a in range(1,len_of_display+1):
+    display.append("_")
+
+print(display)
+# take input of letter from user and convert to lower
+guess = input("Guess a letter: ").lower()
+
+# use the loop to check if guessed (guess) letter matched the randomnly chosed word
+for position in range(len_of_display):
+    letter = chosen_word[position]
+    if letter == guess:
+        display[position] = letter
+        
+print(display)
